7840e8410392cad75774d5093177f90b05588393
Klayton Whited <amber.whited@bryanuniversity.edu>
2021-05-22 12:53:29 -0400

    ajustments

--- Week 2/creatingAndAppendingElements.js
+++ Week 2/creatingAndAppendingElements.js
@@ -1,5 +1,8 @@
-console.log(document);
-var newListItem = document.createElement('h1')
+var newListItem = document.createElement('li')
+var myList = document.getElementById('my-list')
+
 var newP = document.createElement('p')
 newP.textContent = 'All of this was created with Javascript'
 document.body.appendChild(newP)
+
+
