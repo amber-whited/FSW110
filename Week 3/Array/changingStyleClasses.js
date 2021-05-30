8c400b8488502380665af7278304f37eb9cca293
Klayton Whited <amber.whited@bryanuniversity.edu>
2021-05-30 16:46:09 -0400

    assignments

--- Week 3/Array/changingStyleClasses.js
+++ Week 3/Array/changingStyleClasses.js
@@ -1,23 +1,13 @@
-var newH = document.createElement('h2')
-newH.textContent = 'Hello World'
-document.body.append(newH)
-var newH = document.createElement('h2')
-newH.textContent = 'Hello World'
-document.body.append(newH)
-var newH = document.createElement('h2')
-newH.textContent = 'Hello World'
-document.body.append(newH)
-var newH = document.createElement('h2')
-newH.textContent = 'Hello World'
-document.body.append(newH)
-var newH = document.createElement('h2')
-newH.textContent = 'Hello World'
-document.body.append(newH)
-
-var newF = document.querySelectorAll('h1')
-for (var h = 0; h < newF.length; h++){
-    newF[h].style.fontSize = '20px'
-    newF[h].style.fontweight = 'lighter'
-    newF[h].style.fontfamily = 'sans-serif'
-    newF[h].style.color = 'cornflowerblue'
+// var newH = document.querySelectorAll('h2')
+for (var h = 0; h < 6; h++){
+    var newH = document.createElement('h2')
+    newH.textContent = 'Hello World'
+    document.body.append(newH)
+    newH.classList = 'border'
+    newH.style.fontSize = '20px'
+    newH.style.fontweight = 'lighter'
+    newH.style.fontfamily = 'sans-serif'
+    newH.style.color = 'cornflowerblue'
+    
 }
+
