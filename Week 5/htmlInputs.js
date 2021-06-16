2d3d3461d17614fad541c0d3280e5e8c98ec0ee2
Klayton Whited <amber.whited@bryanuniversity.edu>
2021-06-16 15:41:17 -0400

    updates

--- Week 5/htmlInputs.js
+++ Week 5/htmlInputs.js
@@ -1,6 +1,5 @@
 document.forms.addEventListener('submit',function(e){
-    e.preventDefault()
-    console.log(document.forms) 
+
     var fname = document.forms.fname.value
     var lname = document.forms.lname.value
     var age = document.forms.age.value
@@ -8,8 +7,7 @@ document.forms.addEventListener('submit',function(e){
     var dropdown = document.forms.dropdown.value
     var diet = []
     if (document.forms.Keto.checked){
-       
-        diet.push(document.forms.Keto.value)
+       diet.push(document.forms.Keto.value)
     }
     if (document.forms.Kosher.checked){
         diet.push(document.forms.Kosher.value)
@@ -20,11 +18,17 @@ document.forms.addEventListener('submit',function(e){
     if (document.forms.GlutenFree.checked){
         diet.push(document.forms.GlutenFree.value)
     }
-    alert(fname + " " + lname + " " + age + " " + decision + " " + dropdown + " " + diet)
+    alert("First Name: " + fname + "\nLast Name: " + lname + "\nAge: " + age + "\nHave you shopped with us before? " + decision + "\nHow likely are you to recommend? " + dropdown + "\nDo you have any dietary restrictions? " + diet)
     
     
-    let inputs = document.querySelectorAll("input");
-    inputs.forEach((input) => (input.value = ""));
+    // let inputs = document.querySelectorAll("input");
+    // inputs.forEach((input) => (input.value = ""));
+
+    // let options = document.querySelectorAll("label");
+    // options.forEach((label) => (label.value = ""));
+
+    // let labels = document.querySelectorAll("question");
+    // labels.forEach((question) => (question.value = ""));
    
 })
 
